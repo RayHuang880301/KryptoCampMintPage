7da27ca6b813196cba7de641f0d8f2293396f2f1
RayHuang <ray880301@gmail.com>
2022-09-14 23:50:40 +0800

    ignore history/src

--- .history/src/utils_20220913102627.js
+++
@@ -1,4 +0,0 @@
-
-import { InjectedConnector } from 'wagmi/connectors/injected'
-
-export const connector = new InjectedConnector();
--- .history/src/utils_20220913103757.js
+++
@@ -1,3 +0,0 @@
-import { InjectedConnector } from 'wagmi/connectors/injected'
-
-export const connector = new InjectedConnector()
